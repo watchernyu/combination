a890ea54dd89721fc772080995fb7de44c0d9d92
watchernyu <cw1681@nyu.edu>
2023-04-10 01:57:48 -0700

    remove online stuff

--- experiments/train_d4rl.py
+++ experiments/train_d4rl.py
@@ -156,8 +156,6 @@ def redq_sac(env_name='hopper-expert-v2', seed=0, epochs=200, steps_per_epoch=10
             logger.log_tabular('Epoch', epoch)
             logger.log_tabular('TotalEnvInteracts', t)
             logger.log_tabular('Time', time_used)
-            logger.log_tabular('EpRet', with_min_and_max=True)
-            logger.log_tabular('EpLen', average_only=True)
             logger.log_tabular('TestEpRet', with_min_and_max=True)
             logger.log_tabular('TestEpLen', average_only=True)
             logger.log_tabular('Q1Vals', with_min_and_max=True)
